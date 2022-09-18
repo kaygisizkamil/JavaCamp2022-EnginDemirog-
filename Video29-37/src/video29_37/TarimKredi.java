7faafe9e37037d6ad305267a88f7c90ceb9fc82d
kaygisizkamil <kamilkaygisizz@gmail.com>
2022-09-18 16:50:31 +0300

    parametresiz constructor invoke islemi icin ornek class eklendi

---
+++ Video29-37/src/video29_37/TarimKredi.java
@@ -0,0 +1,19 @@
+package video29_37;
+
+public class TarimKredi extends BaseKredi {
+	
+	/*public TarimKredi() {
+		super(a,b,c);
+		}
+	int a;
+	int b;
+	String c;
+	public TarimKredi(int a,int b,String c) {
+		super(a,b,c);
+		}
+		iki declaration da yanlis
+		ve TarimKredi'yi constructorsiz sekilde kullanmak istiyorsak implicit olarak yapilacak BaseKredi constructor cagrisi
+		icin BaseKredi icinde constructor tanýmlamasý yapmalýyýz (parametresiz)
+	*/
+	
+}
