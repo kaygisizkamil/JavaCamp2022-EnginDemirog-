ee168cde5654c8bcdd5b9430bd1645eddbd5adeb
kaygisizkamil <96066271+kaygisizkamil@users.noreply.github.com>
2022-11-15 02:36:36 +0300

    Delete ProgrammingLanguages directory

--- ProgrammingLanguages/src/main/java/com/example/ProgrammingLanguages/dataAccess/concretes/LanguagesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProgrammingLanguages.dataAccess.concretes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ProgrammingLanguages.entities.concretes.Languages;
-
-public interface LanguagesRepository extends JpaRepository<Languages,Integer> {
-				
-}
